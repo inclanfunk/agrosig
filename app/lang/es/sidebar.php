e7ee8a9622f27df2907f745f28655b1ef9a4a67e
inclanfunk <julien.baudet@gmail.com>
2015-03-21 18:07:51 -0300

    Sidebar Translation

---
+++ app/lang/es/sidebar.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+	return [
+		'dashboard' => 'Panel de control',
+
+		'manage' => 'Administra',
+		'list' => 'Lista',
+		'create' => 'Crea',
+
+		'companies' => 'Companias',
+		'sections' => 'Sectiones',
+		'sections' => 'Sectiones',
+
+		'farms' => 'Explotaci&ograve; Agr&igrave;cola',
+		'users' => 'Usuarios',
+		'equipment' => 'Equipo',
+		'orders' => 'Ordenes de Trabajo',
+		'chatrooms' => 'Chatrooms',
+		'parts' => 'Pedido de piezas',
+		'forum' => 'Forum',
+		'topics' => 'Temas',
+		'sections' => 'Sectiones',
+
+		'pivot' => 'Pivote',
+		'waterpump' => 'Bomba',
+
+		'maps' => 'Mapas',
+		'crops' => 'Cultivos',
+		'equipment' => 'Equipo',
+
+		'calendar' => 'Calendario',
+
+		'forum' => 'Forum',
+	];
