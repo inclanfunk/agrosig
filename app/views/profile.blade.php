b752ccc263e2fd0f5db720bb18684632ea1eb8db
Rohan Chhabra <rohan0793@gmail.com>
2015-03-24 23:33:52 +0530

    Small change upload size

--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -301,7 +301,7 @@
 			$("#mydropzone").dropzone({
 				url: "/changePhoto",
 				addRemoveLinks : true,
-				maxFilesize: 20,
+				maxFilesize: 5,
 				dictDefaultMessage: '<span class="text-center"><span class="font-lg visible-xs-block visible-sm-block visible-lg-block"><span class="font-lg"><i class="fa fa-caret-right text-danger"></i> Drop files <span class="font-xs">to upload</span></span><span>&nbsp&nbsp<h4 class="display-inline"> (Or Click)</h4></span>',
 				dictResponseError: 'Error uploading file!'
 			});
