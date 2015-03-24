5ad4d44a52c057ad46839c2701b983945360ddd4
inclanfunk <julien.baudet@gmail.com>
2015-03-24 08:15:24 -0300

    Forum & Sidebar translations

---
+++ app/lang/es/forum.php
@@ -0,0 +1,52 @@
+﻿<?php
+
+	return [
+
+		'me' => 'Yo',
+		'by' => 'por',
+		'na' => 'N/A',
+		'posts' => 'Mensajes',
+		'replies' => 'Respuestas',
+
+		'number_of_posts' => ':contar Mensaje|:contar Mensajes',
+
+		'index' => [
+
+			'other' => 'Otras Paginas',
+			'layout' => 'Foro',
+			'last' => 'Ultimo Mensaje',
+
+		],
+
+		'topic' => [
+
+			'create' => 'Crear nuevo Mensaje',
+			'forum' => 'Foro',
+			'no_posts' => 'Sin Mensajes todav&igrave;a',
+			'first' => 'S&eacute; el primero en publicar!',
+			'last' => 'Ultima Respuesta',
+
+		],
+
+		'post' => [
+
+			'create' => [
+
+				'today' => 'Hoy',
+				'title' => 'Titulo',
+				'required' => 'El titulo y el cuerpo son requeridos!',
+				'post' => 'Mensaje'
+
+			],
+
+			'show' => [
+
+				'now' => 'Ahora',
+				'required' => 'El cuerpo de la respuesta es requerido!',
+				'reply' => 'Contestar'
+
+			],
+
+		],
+		
+	];
