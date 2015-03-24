ac3a2647c0a4f20eac3be955097e82da47af35d6
inclanfunk <julien.baudet@gmail.com>
2015-03-24 07:37:29 -0300

    Calendar translation to spanish

---
+++ app/lang/es/calendar.php
@@ -0,0 +1,30 @@
+﻿?<?php
+
+	return [
+		'calendar' => 'Calendario',
+		'add' => 'A&ntilde;r un Evento',
+		'icon' => 'Elegir un Icono',
+		'title' => 'Titulo del Evento',
+		'description' => 'Descripci&ograve;',
+		'brief' => 'Por favor sea breve',
+		'max' => '40 Caracteres maximo',
+		'color' => 'Elegir un Color',
+		'draggable' => 'Deslizar eventos',
+		'remove' => 'Eliminar despu&egrave;s de usar',
+		'my' => 'Mis Eventos',
+		'showing' => 'Mostrar',
+		'month' => 'Mes',
+		'agenda' => 'Agenda',
+		'today' => 'Hoy',
+
+
+		'office' => 'Seminario Tecno Riego',
+		'busy' => 'Riego eficiente',
+
+		'lunch' => 'Siembra',
+		'no' => 'Maiz Foraje',
+
+		'urgent' => 'Fertilizaci&ograve;'
+		'tasks' => 'Campo 2, nitr&ograve;geno',
+		
+	];
