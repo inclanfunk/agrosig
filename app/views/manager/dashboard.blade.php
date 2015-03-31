6945a2ecb7b487bc706fcc34f7cb9bcb55f02250
Rohan Chhabra <rohan0793@gmail.com>
2015-03-31 15:45:09 +0530

    Map styling in progress

--- app/views/manager/dashboard.blade.php
+++ app/views/manager/dashboard.blade.php
@@ -2,6 +2,49 @@
 
 @section('content')
 
+<style type="text/css">
+
+	#pivotTab ul{
+		list-style-type: none;
+		padding: 0px;
+		margin: 0px;
+	}
+
+	#waterpumpTab ul{
+		list-style-type: none;
+		padding: 0px;
+		margin: 0px;
+	}
+
+	ul#waterpumpGeneral > li:before,
+	ul#pivotGeneral > li:before{
+		font-family: 'FontAwesome';
+		content: '\f01a';
+		margin:0 10px 0 0;
+	}
+
+	ul#waterpumpEbSettings > li:before,
+	ul#pivotEbSettings > li:before{
+		font-family: 'FontAwesome';
+		content: '\f0e7';
+		margin:0 10px 0 0;
+	}
+
+	ul#waterpumpSettings > li:before,
+	ul#pivotSettings> li:before{
+		font-family: 'FontAwesome';
+		content: '\f013';
+		margin:0 10px 0 0;
+	}
+
+	ul#waterpumpDwInfo > li:before{
+		font-family: 'FontAwesome';
+		content: '\f01b';
+		margin:0 10px 0 0;
+	}
+
+</style>
+
 <div class="row">
 	<div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
 		<h1 class="page-title txt-color-blueDark"><i class="fa-fw fa fa-home"></i> Dashboard <span>> My Dashboard</span></h1>
@@ -334,6 +377,67 @@
 						<!-- content goes here -->
 
 						<div id="map" style="width: 100%; height: 300px;"></div>
+
+						<div id="pivotTab"></div>
+
+					</div>
+
+						<div id="waterpumpTab">
+							<ul>
+								<li>
+									<a href="#tabs-a">
+										<i class="fa fa-lg fa-arrow-circle-o-down"></i>
+										<span class="hidden-mobile hidden-tablet">
+											Waterpump
+										</span>
+									</a>
+								</li>
+								<li>
+									<a href="#tabs-b">
+										<i class="fa fa-lg fa-gear"></i>
+										<span class="hidden-mobile hidden-tablet">
+											Settings
+										</span>
+									</a>
+								</li>
+								<li>
+									<a href="#tabs-c">
+										<i class="fa fa-lg fa-bolt"></i>
+										<span class="hidden-mobile hidden-tablet">
+											Electrical Board
+										</span>
+									</a>
+								</li>
+								<li>
+									<a href="#tabs-d">
+										<i class="fa fa-lg fa-arrow-circle-o-up"></i>
+										<span class="hidden-mobile hidden-tablet">
+											Deep Well
+										</span>
+									</a>
+								</li>
+							</ul>
+							<div id="tabs-a">
+								<ul id="waterpumpGeneral">
+									
+								</ul>
+							</div>
+							<div id="tabs-b">
+								<ul id="waterpumpSettings">
+									
+								</ul>
+							</div>
+							<div id="tabs-c">
+								<ul id="waterpumpEbSettings">
+									
+								</ul>
+							</div>
+							<div id="tabs-d">
+								<ul id="waterpumpDwInfo">
+									
+								</ul>
+							</div>
+						</div>
 
 						<!-- end content -->
 
@@ -903,43 +1007,18 @@
 						        'marker-color': '#74DF00'
 						    })
 						}).addTo(pivotsLayer);
-						$('#pivotGeneral').empty()
-						$('#pivotSettings').empty()
-						$('#pivotEbSettings').empty()
-
-						for (var key in pivot_item) {
-							var current = key.split('_');
-							if(current[0] == 'g'){
-								if(current[1] == 'quantity'){
-									current = current.slice(4);
-									current.unshift('QOA');
-								}else{
-									current = current.slice(1);
-								}
-								$.each(current, function(i, item){
-									current[i] = item.charAt(0).toUpperCase() + item.slice(1);
-								});
-								$('#pivotGeneral').append("<li>" + current.join(' ') + ': ' + pivot_item[key] + "</li>");
-							}
-							if(current[0] == 's'){
-								current = current.slice(1);
-								$.each(current, function(i, item){
-									current[i] = item.charAt(0).toUpperCase() + item.slice(1);
-								});
-								$('#pivotSettings').append("<li>" + current.join(' ') + ': ' + pivot_item[key] + "</li>");
-							}
-							if(current[0] == 'eb'){
-								current = current.slice(1);
-								$.each(current, function(i, item){
-									current[i] = item.charAt(0).toUpperCase() + item.slice(1);
-								});
-								$('#pivotEbSettings').append("<li>" + current.join(' ') + ': ' + pivot_item[key] + "</li>");
-							}
-						}
 
 						/*marker.on('click', function(e){
-							$('#wid-id-1').removeClass('hidden');
-							$('#wid-id-2').addClass('hidden');
+							$('div#pivotTab').removeClass('hidden');
+							$('div#waterpumpTab').addClass('hidden');
+
+							$('#pivotTab').empty();
+
+							$('#pivotTab').append('<ul>');
+							$('#pivotTab').append('<li>');
+							$('#pivotTab').append('Hello');
+							$('#pivotTab').append('</li>');
+							$('#pivotTab').append('</ul>');
 						});*/
 					}
 				});
@@ -951,43 +1030,53 @@
 				$.each(data, function(pivot_i, pivot_item){
 					if(pivot_item.lat != ''){
 						var marker = L.marker([parseFloat(pivot_item.lat), parseFloat(pivot_item.long)]).addTo(waterpumpsLayer);
-						$('#waterpumpGeneral').empty()
-						$('#waterpumpSettings').empty()
-						$('#waterpumpEbSettings').empty()
-						for (var key in pivot_item) {
-							var current = key.split('_');
-							if(current[0] == 'g'){
-								current = current.slice(1);
-								$.each(current, function(i, item){
-									current[i] = item.charAt(0).toUpperCase() + item.slice(1);
-								});
-								$('#waterpumpGeneral').append("<li>" + current.join(' ') + ': ' + pivot_item[key] + "</li>");
-							}
-							if(current[0] == 's'){
-								current = current.slice(1);
-								$.each(current, function(i, item){
-									current[i] = item.charAt(0).toUpperCase() + item.slice(1);
-								});
-								$('#waterpumpSettings').append("<li>" + current.join(' ') + ': ' + pivot_item[key] + "</li>");
-							}
-							if(current[0] == 'eb'){
-								current = current.slice(1);
-								$.each(current, function(i, item){
-									current[i] = item.charAt(0).toUpperCase() + item.slice(1);
-								});
-								$('#waterpumpEbSettings').append("<li>" + current.join(' ') + ': ' + pivot_item[key] + "</li>");
-							}
-						}
-						$('#waterpumpDwInfo').empty()
-								.append('<li>Download Info: <a href="/deepwell/' + pivot_item.deepwell_info + '">Download</a></li>')
+						
 						/*marker.on('click', function(e){
-							$('#wid-id-2').removeClass('hidden');
-							$('#wid-id-1').addClass('hidden');
+							$('div#waterpumpTab').removeClass('hidden');
+							$('div#pivotTab').addClass('hidden');
+
+							$('#waterpumpGeneral').empty();
+							$('#waterpumpSettings').empty();
+							$('#waterpumpEbSettings').empty();
+
+							for (var key in pivot_item) {
+								var current = key.split('_');
+								if(current[0] == 'g'){
+									current = current.slice(1);
+									$.each(current, function(i, item){
+										current[i] = item.charAt(0).toUpperCase() + item.slice(1);
+									});
+									$('#waterpumpGeneral').append("<li>" + current.join(' ') + ': ' + pivot_item[key] + "</li>");
+								}
+								if(current[0] == 's'){
+									current = current.slice(1);
+									$.each(current, function(i, item){
+										current[i] = item.charAt(0).toUpperCase() + item.slice(1);
+									});
+									$('#waterpumpSettings').append("<li>" + current.join(' ') + ': ' + pivot_item[key] + "</li>");
+								}
+								if(current[0] == 'eb'){
+									current = current.slice(1);
+									$.each(current, function(i, item){
+										current[i] = item.charAt(0).toUpperCase() + item.slice(1);
+									});
+									$('#waterpumpEbSettings').append("<li>" + current.join(' ') + ': ' + pivot_item[key] + "</li>");
+								}
+							}
+							$('#waterpumpDwInfo').empty()
+									.append('<li>Download Info: <a href="/deepwell/' + pivot_item.deepwell_info + '">Download</a></li>');
 						});*/
 					}
 				});
 			});
 		}
+
+		$('div#pivotTab').tabs();
+		$('div#waterpumpTab').tabs();
+
+		$('div#pivotTab').addClass('hidden');
+		$('div#waterpumpTab').addClass('hidden');
+
 	});
 
 </script>
