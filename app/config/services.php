a6fa308ab7f13ac9da8651e2d0372dd2780b0b18
Rohan Chhabra <rohan0793@gmail.com>
2015-04-16 10:31:04 +0530

    Mail shifted from SMTP to Mailgun API

--- app/config/services.php
+++ app/config/services.php
@@ -15,8 +15,8 @@ return array(
 	*/
 
 	'mailgun' => array(
-		'domain' => '',
-		'secret' => '',
+		'domain' => getenv('mailgun.domain'),
+		'secret' =>  getenv('mailgun.secret'),
 	),
 
 	'mandrill' => array(
